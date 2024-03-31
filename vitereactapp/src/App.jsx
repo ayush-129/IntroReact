c45007d4af3ba392770159826a877733e1a22e42
Ayush Kumar <140801177+ayush-129@users.noreply.github.com>
2024-03-31 17:08:10 +0530

    custom_testing

--- vitereactapp/src/App.jsx
+++ vitereactapp/src/App.jsx
@@ -1,12 +1,16 @@
 import Chai from "./comp";
 
 function App() {
+  const username="Ayush is here"
   return (
     <>
-      <h1>Vite React App</h1>
+      <h1>Vite React App, {username}</h1>
       <Chai/>
     </>
   );
 }
 
 export default App;
+
+
+// {username} is the evualated expression. We cant write javascript inside the return 
--- vitereactapp/src/main.jsx
+++ vitereactapp/src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     App()
+// );
+// ----- OR ------
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+    <App/>
+);
 
-)
+// function Myapp(){
+//     return <h3>Custom Testing</h3>
+// }
+// ReactDOM.createRoot(document.getElementById('root')).render(
+  
+//     Myapp()
+
+// )
+
+
+// const anotherElement = (
+//     <a href="https://google.com" target="_blank">Visit Google</a>
+//   );
+  
+//   ReactDOM.createRoot(document.getElementById('root')).render(
+//     anotherElement
+//   );
+
+
+// const anotherUser="another testing"
+// const reactElement=React.createElement(
+//     'a',
+//     {href:'https://google.com', target: '_blank'},
+//     'click to visit goggle',
+//     anotherUser
+// )
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     reactElement
+// );
+
+
+
+
+
+  
